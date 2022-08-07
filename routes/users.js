c2347ddde61f2cbd9570c675a3349a3ba54e1d65
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2022-08-07 13:20:22 +0530

    delete user

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,7 @@ router.put("/:id", async (req, res) => {
   const id = req.params.id;
   const userId = req.body.userId;
 
-  if (userId === id || req.user.isAdmin) {
+  if (userId === id || req.body.isAdmin) {
     if (req.body.password) {
       try {
         const salt = await bcrypt.genSalt(10);
@@ -32,7 +32,24 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-//delete a user
+// @Route    DELETE api/users/:id
+// @desc     delete user
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  const userId = req.body.userId;
+
+  if (userId === id || req.body.isAdmin) {
+    try {
+      await User.deleteOne({ _id: id });
+      res.status(200).json({ msg: "Account has been deleted successfully" });
+    } catch (error) {
+      return res.status(500).json({ msg: "server error" });
+    }
+  } else {
+    return res.status(403).json({ msg: "Account cannot be deleted" });
+  }
+});
+
 // get a user
 // follow a user
 // unfollow a user
