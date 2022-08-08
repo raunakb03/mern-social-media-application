08ec070db2e550b773fbde5874044f2a14b7e3e8
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2022-08-08 17:41:12 +0530

    sidebar and share post component

---
+++ client/src/components/feed/Feed.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './feed.css'
+import Share from '../share/Share'
+
+function Feed() {
+  return (
+    <div className='feed'>
+      <div className="feedWrapper">
+<Share/>
+      </div>
+    </div>
+  )
+}
+
+export default Feed
