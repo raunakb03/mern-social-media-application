1733500641549d1bdf8c99b5b69c4e1b2bba3038
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2022-08-13 09:28:23 +0530

    final push(not include messaging)

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,34 @@
 import Home from "./pages/home/Home";
-import { Person } from "@mui/icons-material";
-import Profile from "./pages/profile/Profile";
 import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  const { user } = useContext(AuthContext);
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element= {<Home/>}/>
-        <Route path="/login" element= {<Login/>}/>
-        <Route path="/register" element= {<Register/>}/>
-        <Route path="/profile/:id" element= {<Profile/>}/>
-      </Routes>
-    </BrowserRouter>
-    );
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {user ? <Home /> : <Register />}
+        </Route>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
+        <Route path="/register">
+          {user ? <Redirect to="/" /> : <Register />}
+        </Route>
+        <Route path="/profile/:username">
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
